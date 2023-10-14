ef424d4b50723b90def14f1f3fab3ded027bf3cc
Jay Brydon <jbr268@uclive.ac.nz>
2023-10-14 15:53:54 +1300

    random_number_generator

---
+++ blue_led_timer.c
@@ -0,0 +1,9 @@
+/** @ random_number_generator.h
+ *  @ Author: Jay Brydon
+ *  @ 14 October 2023
+ *  @ The code for the timer that controls the rate at which the blue LED flashes.
+*/
+
+#include "blue_led_timer.h"
+#include <avr/io.h>
+
---
+++ blue_led_timer.h
@@ -0,0 +1,12 @@
+/** @ random_number_generator.h
+ *  @ Author: Jay Brydon
+ *  @ 14 October 2023
+ *  @ The code for the timer that controls the rate at which the blue LED flashes.
+*/
+
+#ifndef BLUELEDTIMER_H
+#define BLUELEDTIMER_H
+
+#include "system.h"
+
+#endif /* BLUELEDTIMER_H */
